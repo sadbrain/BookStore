e37f65e99267746b9333020b147ea08991c2594c
sadbrain <riotgheptrannhulol@gmail.com>
2024-02-16 00:34:32 -0800

    feat: send email wirh not secret key

--- BookStore.Utility/EmailSender.cs
+++ BookStore.Utility/EmailSender.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +11,18 @@ namespace BookStore.Utility;
 
 public class EmailSender : IEmailSender
 {
+    public string SendGridSecret { get; set; }
+    public EmailSender(IConfiguration _config)
+    {
+        SendGridSecret = _config.GetValue<string>("SendGrid:SecretKey");
+    }
     public Task SendEmailAsync(string email, string subject, string htmlMessage)
     {
         //logic to send email
-        return Task.CompletedTask;
+        var client = new SendGridClient(SendGridSecret);
+        var from = new EmailAddress("riotgheptrannhulol@gmail.com", "BookStore");
+        var to = new EmailAddress(email);
+        var message = MailHelper.CreateSingleEmail(from, to, subject, "", htmlMessage);
+        return client.SendEmailAsync(message);
     }
 }
